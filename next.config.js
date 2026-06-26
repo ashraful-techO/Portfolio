a1dfce52430ea4cbc39c0b6cba811336a840a1e5
ashraful-techO <islam.ashraful.cse.97@gmail.com>
2026-06-27 00:04:51 +0600

    feat: enable static exports in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
   experimental: {
     scrollRestoration: true,
   },
+
   env: {
     EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
   },
